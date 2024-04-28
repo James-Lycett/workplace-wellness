064b80c5274a4efe4e203a2196bef2945b23e674
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-04-28 19:48:36 -0400

    Attempted to import Roboto font
    But I wasn't successful. I copied the import call into the top of index.css and then rebuilt the tailwind, but it's clearly just using the backup and not roboto
    
    Do you know what I did wrong?

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,61 +1,58 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js"
-  ],
-  theme: {
-    fontFamily: {
-      "arial": ["Arial", "ui-sans-serif", "ui-serif", "system-ui"]
-    },
-    fontSize: {
-      sm: '0.8rem',
-      base: '1rem',
-      xl: '1.25rem',
-      'midxl': '1.35rem',
-      '2xl': '1.563rem',
-      '3xl': '1.953rem',
-      '4xl': '2.441rem',
-      '5xl': '3.052rem',
-    },
-    fontWeight: {
-      thin: '100',
-      hairline: '100',
-      extralight: '200',
-      light: '300',
-      normal: '400',
-      medium: '500',
-      semimedium: '550',
-      semibold: '600',
-      bold: '700',
-      extrabold: '800',
-      'extra-bold': '800',
-      black: '900',
-    },
-    extend: {
-      colors: {
-        v2: {
-          ltblue: '#38B6FF',
-          drkblue: '#0B1997'
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}',
+        'node_modules/flowbite-react/lib/esm/**/*.js',
+    ],
+    theme: {
+        fontFamily: {
+            roboto: ['Roboto', 'ui-sans-serif'],
         },
-        primary: {
-          1: '#000000',
-          2: '#FFFFFF',
-          3: '#0B1997',
-          4: '#00528E',
-          5: '#FFFFFF',
+        fontSize: {
+            sm: '0.8rem',
+            base: '1rem',
+            xl: '1.25rem',
+            midxl: '1.35rem',
+            '2xl': '1.563rem',
+            '3xl': '1.953rem',
+            '4xl': '2.441rem',
+            '5xl': '3.052rem',
         },
-        accent: {
-          1: '#51565A',
-          2: '#022860',
-          3: '#F0F0F0',
-          background: '#E2EFF2'
+        fontWeight: {
+            thin: '100',
+            hairline: '100',
+            extralight: '200',
+            light: '300',
+            normal: '400',
+            medium: '500',
+            semimedium: '550',
+            semibold: '600',
+            bold: '700',
+            extrabold: '800',
+            'extra-bold': '800',
+            black: '900',
         },
-      },
+        extend: {
+            colors: {
+                v2: {
+                    ltblue: '#38B6FF',
+                    drkblue: '#0B1997',
+                },
+                primary: {
+                    1: '#000000',
+                    2: '#FFFFFF',
+                    3: '#0B1997',
+                    4: '#00528E',
+                    5: '#FFFFFF',
+                },
+                accent: {
+                    1: '#51565A',
+                    2: '#022860',
+                    3: '#F0F0F0',
+                    background: '#E2EFF2',
+                },
+            },
+        },
     },
-  },
-  plugins: [
-    require("flowbite/plugin")
-  ],
+    plugins: [require('flowbite/plugin')],
 }
-
