4e2f00658fbc7b03eca9d7cf53b893aba41b1802
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-04-28 20:46:49 -0400

    adjust spacing on call to action
    button and p and m's

--- frontend/src/LandingPage/CallToActionBlock.jsx
+++ frontend/src/LandingPage/CallToActionBlock.jsx
@@ -1,11 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function CallToActionBlock() {
     return (
-        <div className="lg:w-4/5 text-center lg:text-midxl font-medium lg:p-12 my-12 lg:bg-white lg:rounded-xl">
-            <p className="mb-5">Are you ready to wellness up your workplace? Click here to register an account and get your workness wellplace journey started!</p>
-            <Link to="/register" className="button-light-blue">Get Started</Link>
+        <div className="lg:w-4/5 text-center lg:text-midxl font-medium lg:p-12 my-8 lg:bg-white lg:rounded-xl">
+            <p className="mb-10">
+                Are you ready to wellness up your workplace? Click here to
+                register an account and get your workness wellplace journey
+                started!
+            </p>
+            <div className="py-3">
+                <Link to="/register" className="button-light-blue">
+                    Get Started
+                </Link>
+            </div>
         </div>
     )
 }
