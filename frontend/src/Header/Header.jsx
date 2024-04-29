17eea7e2cb7b96b978902db5349522f6b5a4661e
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-04-28 20:12:32 -0400

    removed pb-10 from header

--- frontend/src/Header/Header.jsx
+++ frontend/src/Header/Header.jsx
@@ -1,40 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "./logo.png"
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from './logo.png'
 
 function Header() {
     return (
-        <header className="bg-white text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200">
-            <div className="flex flex-col lg:flex-row lg:justify-between lg:pe-16 pb-10">
+        <header className="bg-white text-center text-neutral-600 border-b-1 border-slate-500 dark:bg-neutral-600 dark:text-neutral-200">
+            <div className="flex flex-col lg:flex-row lg:justify-between lg:pe-16">
                 <div className="flex flex-col mx-12">
-                    <Link 
-                        to="/"
-                    >
-                        <img src={logo} alt="Logo" className="m-auto lg:ms-3 lg:w-1/2" />
+                    <Link to="/">
+                        <img
+                            src={logo}
+                            alt="Logo"
+                            className="m-auto lg:ms-3 lg:w-1/2"
+                        />
                     </Link>
                 </div>
                 <nav className="font-semibold lg:flex lg:items-center">
                     <ul className="flex flex-col gap-4 lg:flex lg:flex-row lg:gap-6 text-blue-800 dark:text-neutral-200">
                         <li className=" hover:text-blue-500 dark:hover:text-blue-400">
-                            <Link
-                                to="/bp/contact"
-                            >
-                                Contact
-                            </Link>
+                            <Link to="/bp/contact">Contact</Link>
                         </li>
                         <li className="hover:text-blue-500 dark:hover:text-blue-400">
-                            <Link
-                                to="/bp/about"
-                            >
-                                About
-                            </Link>
+                            <Link to="/bp/about">About</Link>
                         </li>
                         <li className="hover:text-blue-500 dark:hover:text-blue-400">
-                            <Link
-                                to="/login"
-                            >
-                                Sign In
-                            </Link>
+                            <Link to="/login">Sign In</Link>
                         </li>
                     </ul>
                 </nav>
