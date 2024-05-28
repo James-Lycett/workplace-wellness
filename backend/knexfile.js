74ff431dd29478caa734f2ae446338e3f2d3d525
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-05-28 14:44:29 -0400

    Testing Company Averages, Adjusting routes

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,12 +1,12 @@
 // Update with your config settings.
-require("dotenv").config()
+require('dotenv').config()
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
     development: {
-        client: "postgresql",
+        client: 'postgresql',
         connection: {
             host: process.env.SQL_CREDENTIALS_HOST,
             user: process.env.SQL_CREDENTIALS_USER_DB,
@@ -15,15 +15,15 @@ module.exports = {
             ssl: true, // Enable SSL for secure connections
         },
         migrations: {
-            tableName: "knex_migrations",
-            directory: "src/db/migrations",
+            tableName: 'knex_migrations',
+            directory: 'src/db/migrations',
         },
         seeds: {
-            directory: "src/db/seeds",
+            directory: 'src/db/seeds',
         },
     },
     production: {
-        client: "postgresql",
+        client: 'postgresql',
         connection: {
             host: process.env.SQL_CREDENTIALS_HOST,
             user: process.env.SQL_CREDENTIALS_USER_DB,
@@ -32,11 +32,11 @@ module.exports = {
             ssl: true, // Enable SSL for secure connections
         },
         migrations: {
-            tableName: "knex_migrations",
-            directory: "src/db/migrations",
+            tableName: 'knex_migrations',
+            directory: 'src/db/migrations',
         },
         seeds: {
-            directory: "src/db/seeds",
+            directory: 'src/db/seeds',
         },
     },
 }
--- frontend/src/AdminHome/AdminHome.jsx
+++ frontend/src/AdminHome/AdminHome.jsx
@@ -17,9 +17,12 @@ import {
     HiUserGroup,
     HiDocumentReport,
 } from 'react-icons/hi'
-import { readUserById, readAveragesById, readCompanyMetrics } from '../utils/api'
+import {
+    readUserById,
+    readAveragesById,
+    readCompanyMetrics,
+} from '../utils/api'
 import Spinner from '../utils/Spinner'
-
 
 export default function AdminHome() {
     const { userId } = useParams()
@@ -56,14 +59,13 @@ export default function AdminHome() {
         },
     ]
 
-
     const [user, setUser] = useState(null)
     const [averages, setAverages] = useState({
         sleep_duration_average: 0,
         daily_steps_average: 0,
         stress_level_average: 0,
         heart_rate_average: 0,
-        bmi_category_average: "N/A"
+        bmi_category_average: 'N/A',
     })
     const [companyMetrics, setCompanyMetrics] = useState({
         sleep_duration_total: 0,
@@ -75,15 +77,22 @@ export default function AdminHome() {
         const abortController = new AbortController()
 
         try {
-            const readUserResponse = await readUserById(userId, abortController.signal)
-            const readAveragesResponse = await readAveragesById(userId, abortController.signal)
+            const readUserResponse = await readUserById(
+                userId,
+                abortController.signal
+            )
+            const readAveragesResponse = await readAveragesById(
+                userId,
+                abortController.signal
+            )
             setAverages(readAveragesResponse)
             setUser(readUserResponse)
             if (readUserResponse.admin) {
-                const readCompanyMetricsResponse = await readCompanyMetrics(abortController.signal)
+                const readCompanyMetricsResponse = await readCompanyMetrics(
+                    abortController.signal
+                )
                 setCompanyMetrics(readCompanyMetricsResponse)
             }
-
         } catch (error) {
             console.error(error)
         } finally {
@@ -91,21 +100,26 @@ export default function AdminHome() {
         }
     }, [userId])
 
-
     useEffect(() => {
         loadData()
     }, [loadData, userId])
 
-
     function calculateSleepHoursProgress() {
-        const progressValue = (companyMetrics.sleep_duration_total / goals.sleepHoursGoal) * 100;
-        const boundedProgressValue = Math.min(Math.max(progressValue, 0), 100);
-    
+        const progressValue =
+            (companyMetrics.sleep_duration_total / goals.sleepHoursGoal) * 100
+        const boundedProgressValue = Math.min(Math.max(progressValue, 0), 100)
+
         return Math.floor(boundedProgressValue) // Return the progress value
     }
 
+    const sleepHourProg = calculateSleepHoursProgress()
+    console.log(averages.sleep_duration_average)
+
     function calculateSleepQualityProgress() {
-        return (companyMetrics.quality_of_sleep_average / goals.sleepQualityGoal) * 100
+        return (
+            (companyMetrics.quality_of_sleep_average / goals.sleepQualityGoal) *
+            100
+        )
     }
 
     function renderConditionsMet() {
@@ -117,7 +131,7 @@ export default function AdminHome() {
         }
     }
 
-/*
+    /*
     was just using this jenky lil thing instead of console.logs to prove my data was showing up
     cause I had some trouble logging async promise stuff from directly in the loadData function
     figured I'll keep it around for now
@@ -137,138 +151,149 @@ export default function AdminHome() {
     }
 */
     if (renderConditionsMet()) {
-    return (
-        <>
-            <section className="bg-slate-100 py-5">
-                {/*{user ? generateElements() : null}*/}
-                <div className="flex flex-col justify-center bg-white w-full max-w-5xl mx-auto rounded-md shadow-md">
-                    <h2 className="text-v2-drkblue font-semibold self-center mt-5">
-                        Department Goals
-                    </h2>
-                    <Progress
-                        progress={70}
-                        color="blue"
-                        progressLabelPosition="inside"
-                        className="mt-10 mb-5 max-w-4xl mx-auto"
-                        size="xl"
-                        labelProgress
-                    />
-                </div>
-                <div className="flex flex-row justify-between my-5 mx-auto max-w-5xl font-light">
-                    <div className="flex flex-col justify-center bg-white me-5 rounded-lg shadow-md">
-                        <RadialBar
-                            series={[calculateSleepHoursProgress()]}
-                            labels={['Sleep Hours']}
-                            colors={['#7AEB7F']}
+        return (
+            <>
+                <section className="bg-slate-100 py-5">
+                    {/*{user ? generateElements() : null}*/}
+                    <div className="flex flex-col justify-center bg-white w-full max-w-5xl mx-auto rounded-md shadow-md">
+                        <h2 className="text-v2-drkblue font-semibold self-center mt-5">
+                            Department Goals
+                        </h2>
+                        <Progress
+                            progress={70}
+                            color="blue"
+                            progressLabelPosition="inside"
+                            className="mt-10 mb-5 max-w-4xl mx-auto"
+                            size="xl"
+                            labelProgress
                         />
                     </div>
-                    <div className="flex flex-col justify-center bg-white mx-10 rounded-lg shadow-md">
-                        <RadialBar
-                            series={[30]}
-                            labels={['Sleep Quality']}
-                            colors={['#EB897A']}
-                        />
+                    <div className="flex flex-row justify-between my-5 mx-auto max-w-5xl font-light">
+                        <div className="flex flex-col justify-center bg-white me-5 rounded-lg shadow-md">
+                            <RadialBar
+                                series={[{ sleepHourProg }]}
+                                labels={['Sleep Hours']}
+                                colors={['#7AEB7F']}
+                            />
+                        </div>
+                        <div className="flex flex-col justify-center bg-white mx-10 rounded-lg shadow-md">
+                            <RadialBar
+                                series={[30]}
+                                labels={['Sleep Quality']}
+                                colors={['#EB897A']}
+                            />
+                        </div>
+                        <div className="flex flex-col justify-center bg-white ms-5 rounded-lg shadow-md">
+                            <RadialBar
+                                series={[45]}
+                                labels={['Tasks Completed']}
+                                colors={['#E8EA8B']}
+                            />
+                        </div>
                     </div>
-                    <div className="flex flex-col justify-center bg-white ms-5 rounded-lg shadow-md">
-                        <RadialBar
-                            series={[45]}
-                            labels={['Tasks Completed']}
-                            colors={['#E8EA8B']}
-                        />
-                    </div>
-                </div>
-                <div className=" flex flex-row w-full mx-auto mt-5 max-w-5xl max-h-[50vh] rounded-lg shadow-md overflow-hidden ">
-                    <div>
-                        <Sidebar aria-label="Default sidebar example">
-                            <Sidebar.Logo
-                                href="#"
-                                img="/circle.png"
-                                imgAlt="Workplace Wellness logo"
-                            >
-                                Workplace Wellness
-                            </Sidebar.Logo>
-                            <Sidebar.Items className="flex flex-col justify-between h-[43vh]">
-                                <div>
-                                    <Sidebar.ItemGroup className="">
-                                        <Sidebar.Item
-                                            href="#"
-                                            icon={HiOutlineIdentification}
-                                        >
-                                            Profile
-                                        </Sidebar.Item>
-                                        <Sidebar.Item href="#" icon={HiTable}>
-                                            My Logs
-                                        </Sidebar.Item>
-                                        <Sidebar.Collapse
-                                            icon={HiUserGroup}
-                                            label="Users"
-                                        >
+                    <div className=" flex flex-row w-full mx-auto mt-5 max-w-5xl max-h-[50vh] rounded-lg shadow-md overflow-hidden ">
+                        <div>
+                            <Sidebar aria-label="Default sidebar example">
+                                <Sidebar.Logo
+                                    href="#"
+                                    img="/circle.png"
+                                    imgAlt="Workplace Wellness logo"
+                                >
+                                    Workplace Wellness
+                                </Sidebar.Logo>
+                                <Sidebar.Items className="flex flex-col justify-between h-[43vh]">
+                                    <div>
+                                        <Sidebar.ItemGroup className="">
                                             <Sidebar.Item
                                                 href="#"
-                                                icon={HiChartPie}
+                                                icon={HiOutlineIdentification}
                                             >
-                                                Statistics
+                                                Profile
                                             </Sidebar.Item>
                                             <Sidebar.Item
                                                 href="#"
-                                                icon={HiLightningBolt}
+                                                icon={HiTable}
                                             >
-                                                Goals
+                                                My Logs
                                             </Sidebar.Item>
-                                            <Sidebar.Item href="#" icon={HiBan}>
-                                                Inactive
+                                            <Sidebar.Collapse
+                                                icon={HiUserGroup}
+                                                label="Users"
+                                            >
+                                                <Sidebar.Item
+                                                    href="#"
+                                                    icon={HiChartPie}
+                                                >
+                                                    Statistics
+                                                </Sidebar.Item>
+                                                <Sidebar.Item
+                                                    href="#"
+                                                    icon={HiLightningBolt}
+                                                >
+                                                    Goals
+                                                </Sidebar.Item>
+                                                <Sidebar.Item
+                                                    href="#"
+                                                    icon={HiBan}
+                                                >
+                                                    Inactive
+                                                </Sidebar.Item>
+                                            </Sidebar.Collapse>
+                                        </Sidebar.ItemGroup>
+                                    </div>
+                                    <div>
+                                        <Sidebar.ItemGroup>
+                                            <Sidebar.Item
+                                                href="#"
+                                                icon={HiDocumentReport}
+                                            >
+                                                SEE REPORT
                                             </Sidebar.Item>
-                                        </Sidebar.Collapse>
-                                    </Sidebar.ItemGroup>
-                                </div>
-                                <div>
-                                    <Sidebar.ItemGroup>
-                                        <Sidebar.Item
-                                            href="#"
-                                            icon={HiDocumentReport}
-                                        >
-                                            SEE REPORT
-                                        </Sidebar.Item>
-                                        <Sidebar.Item
-                                            href="#"
-                                            icon={HiOutlinePlus}
-                                        >
-                                            Add A New Employee
-                                        </Sidebar.Item>
-                                        <Sidebar.Item href="#" icon={HiLogout}>
-                                            Sign Out
-                                        </Sidebar.Item>
-                                    </Sidebar.ItemGroup>
-                                </div>
-                            </Sidebar.Items>
-                        </Sidebar>
+                                            <Sidebar.Item
+                                                href="#"
+                                                icon={HiOutlinePlus}
+                                            >
+                                                Add A New Employee
+                                            </Sidebar.Item>
+                                            <Sidebar.Item
+                                                href="#"
+                                                icon={HiLogout}
+                                            >
+                                                Sign Out
+                                            </Sidebar.Item>
+                                        </Sidebar.ItemGroup>
+                                    </div>
+                                </Sidebar.Items>
+                            </Sidebar>
+                        </div>
+                        <div className="overflow-auto">
+                            <Table hoverable>
+                                <Table.Head className="sticky top-0 bg-white z-10">
+                                    <Table.HeadCell>
+                                        <Checkbox />
+                                    </Table.HeadCell>
+                                    <Table.HeadCell>User</Table.HeadCell>
+                                    <Table.HeadCell>Age</Table.HeadCell>
+                                    <Table.HeadCell>
+                                        Stress Level
+                                    </Table.HeadCell>
+                                    <Table.HeadCell>Sleep Hours</Table.HeadCell>
+                                    <Table.HeadCell>
+                                        <span className="sr-only">Edit</span>
+                                    </Table.HeadCell>
+                                    <Table.HeadCell>
+                                        <span className="sr-only">X</span>
+                                    </Table.HeadCell>
+                                </Table.Head>
+                                <Table.Body className="divide-y">
+                                    <EmployeesListNew />
+                                </Table.Body>
+                            </Table>
+                        </div>
                     </div>
-                    <div className="overflow-auto">
-                        <Table hoverable>
-                            <Table.Head className="sticky top-0 bg-white z-10">
-                                <Table.HeadCell>
-                                    <Checkbox />
-                                </Table.HeadCell>
-                                <Table.HeadCell>User</Table.HeadCell>
-                                <Table.HeadCell>Age</Table.HeadCell>
-                                <Table.HeadCell>Stress Level</Table.HeadCell>
-                                <Table.HeadCell>Sleep Hours</Table.HeadCell>
-                                <Table.HeadCell>
-                                    <span className="sr-only">Edit</span>
-                                </Table.HeadCell>
-                                <Table.HeadCell>
-                                    <span className="sr-only">X</span>
-                                </Table.HeadCell>
-                            </Table.Head>
-                            <Table.Body className="divide-y">
-                                <EmployeesListNew />
-                            </Table.Body>
-                        </Table>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+                </section>
+            </>
+        )
     } else {
         return <Spinner />
     }
