ea7004d2dd2f5c05d2e9422a6cd75fb65b26353e
James-Lycett <jrlycett@mail.com>
2024-06-02 10:14:06 -0700

    finished sidebar links

--- frontend/src/Dashboard/DashboardSidebar/DashboardSidebar.jsx
+++ frontend/src/Dashboard/DashboardSidebar/DashboardSidebar.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import { Sidebar } from 'flowbite-react'
 import {
-    HiBan,
-    HiChartPie,
-    HiLightningBolt,
     HiLogout,
     HiOutlinePlus,
     HiOutlineIdentification,
     HiTable,
-    HiUserGroup,
     HiDocumentReport,
+    HiAcademicCap,
+    HiEmojiSad,
+    HiSpeakerphone,
+    HiLightBulb,
 } from 'react-icons/hi'
 import logo from './circle.svg'
 
-export default function DashboardSidebar({ openModal, openAddEmployeeModal, userIsAdmin, view, setView }) {
-
+export default function DashboardSidebar({
+    openModal,
+    userIsAdmin,
+    view,
+    setView,
+}) {
+    // Necessary to keep page from jumping to top when view is toggled
     const handleNavigation = (e, newView) => {
-        e.preventDefault();
-        setView(newView);
-        // Perform any other necessary state updates here
-    };
+        e.preventDefault()
+        setView(newView)
+    }
 
     return (
         <>
@@ -34,74 +38,82 @@ export default function DashboardSidebar({ openModal, openAddEmployeeModal, user
                     </Sidebar.Logo>
                     <Sidebar.Items className="flex flex-col justify-between h-[43vh]">
                         <div>
-                            <Sidebar.ItemGroup className="">
-                                {userIsAdmin ?
-                                <>
-                                    <Sidebar.Item
-                                        href="#"
-                                        icon={HiOutlineIdentification}
-                                        onClick={(e) => handleNavigation(e, "user")}
-                                    >
-                                        Profile
-                                    </Sidebar.Item>
-                                    <Sidebar.Item
-                                        href="#"
-                                        icon={HiTable}
-                                        onClick={(e) => handleNavigation(e, "admin")}
-                                    >
-                                        Employees
-                                    </Sidebar.Item>
-                                    <Sidebar.Collapse
-                                    icon={HiUserGroup}
-                                    label="Users"
-                                    >
-                                        <Sidebar.Item href="#" icon={HiChartPie}>
-                                            Statistics
+                            <Sidebar.ItemGroup>
+                                {userIsAdmin ? (
+                                    <>
+                                        <Sidebar.Item
+                                            href="#"
+                                            icon={HiOutlineIdentification}
+                                            onClick={(e) =>
+                                                handleNavigation(e, 'user')
+                                            }
+                                        >
+                                            Profile
                                         </Sidebar.Item>
                                         <Sidebar.Item
                                             href="#"
-                                            icon={HiLightningBolt}
+                                            icon={HiTable}
+                                            onClick={(e) =>
+                                                handleNavigation(e, 'admin')
+                                            }
                                         >
-                                            Goals
+                                            Employees
                                         </Sidebar.Item>
-                                        <Sidebar.Item href="#" icon={HiBan}>
-                                            Inactive
+                                    </>
+                                ) : null}
+                                {view === 'user' ? (
+                                    <>
+                                        <Sidebar.Item
+                                            href="#"
+                                            onClick={() =>
+                                                openModal('activity')
+                                            }
+                                            icon={HiDocumentReport}
+                                        >
+                                            LOG NEW ACTIVITY
                                         </Sidebar.Item>
-                                    </Sidebar.Collapse>
-                                </>
-                                    : null
-                                }
-                                <Sidebar.Item href="#" icon={HiTable}>
-                                    My Logs
-                                </Sidebar.Item>
-                                
-                            </Sidebar.ItemGroup>
-                        </div>
-                        <div>
-                            <Sidebar.ItemGroup>
-                                {view === "user" ?
-                                    <Sidebar.Item
-                                    href="#"
-                                    onClick={() => openModal("activity")}
-                                    icon={HiDocumentReport}
-                                    >
-                                        LOG NEW ACTIVITY
-                                    </Sidebar.Item>
-                                    : null
-                                }
-                                {view === "admin" ?
+                                        <Sidebar.Collapse
+                                            icon={HiAcademicCap}
+                                            label="Resources"
+                                        >
+                                            <Sidebar.Item
+                                                href="/tips/sleep"
+                                                target="_blank"
+                                                icon={HiEmojiSad}
+                                            >
+                                                Sleep Disorders Guide
+                                            </Sidebar.Item>
+                                            <Sidebar.Item
+                                                href="/tips/fitness"
+                                                target="_blank"
+                                                icon={HiSpeakerphone}
+                                            >
+                                                Physical Fitness Guide
+                                            </Sidebar.Item>
+                                            <Sidebar.Item
+                                                href="/tips/meditation"
+                                                target="_blank"
+                                                icon={HiLightBulb}
+                                            >
+                                                Meditation Guide
+                                            </Sidebar.Item>
+                                        </Sidebar.Collapse>
+                                    </>
+                                ) : null}
+                                {view === 'admin' ? (
                                     <Sidebar.Item
                                         href="#"
                                         icon={HiOutlinePlus}
-                                        onClick={() => openModal("employee")}
+                                        onClick={() => openModal('employee')}
                                     >
                                         Add A New Employee
                                     </Sidebar.Item>
-                                    : null
-                                }
-                                <Sidebar.Item href="#" icon={HiLogout}>
-                                    Sign Out
-                                </Sidebar.Item>
+                                ) : null}
+                                <Sidebar.ItemGroup>
+                                    <Sidebar.Item href="/" icon={HiLogout}>
+                                        Sign Out
+                                    </Sidebar.Item>
+                                </Sidebar.ItemGroup>
                             </Sidebar.ItemGroup>
                         </div>
                     </Sidebar.Items>
