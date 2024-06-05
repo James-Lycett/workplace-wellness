8d6c32e068fb31c444e66a075845a1664023e550
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-06-05 12:12:26 -0400

    tweak the copy and add our names

--- frontend/src/Boilerplate/About.jsx
+++ frontend/src/Boilerplate/About.jsx
@@ -6,17 +6,6 @@ const About = () => {
             <section className="max-w-screen-md mx-4 md:mx-auto my-8 p-6 bg-white">
                 <div className="bg-gray-700 text-white p-6 mb-10 rounded-lg text-sm font-light inset-lg">
                     <p className="mb-4">
-                        Workplace Wellness is a web application for tracking and
-                        improving company-wide sleep health both for employees
-                        and employers. Users can input daily sleep and health
-                        metrics (eg., sleep duration, blood pressure, stress
-                        level) and view past reports or check out recommended
-                        strategies for improving their sleep health. Admins get
-                        a bird's eye view of the entire company's sleep health
-                        and can add or delete users as employees come and go
-                        from the company.
-                    </p>
-                    <p>
                         This project was originally developed as a team project
                         for the{' '}
                         <span className="font-bold">
@@ -25,9 +14,23 @@ const About = () => {
                         . The team was comprised of three software engineers, a
                         UI/UX designer, a data analyst, and a senior advisor.
                         The repo you are looking at is the product of two of the
-                        team engineers' later efforts to re-engineer the
-                        codebase with an emphasis on refactoring and modernizing
-                        the UI/UX.
+                        team engineers' (
+                        <span className="font-bold">
+                            James Lycett and Greg Topscher
+                        </span>
+                        ) later efforts to re-engineer the codebase with an
+                        emphasis on refactoring and modernizing the UI/UX.
+                    </p>
+                    <p>
+                        Workplace Wellness is a web application for tracking and
+                        improving company-wide sleep health, both for employees
+                        and employers. Users can input daily sleep and health
+                        metrics (eg. sleep duration, blood pressure, stress
+                        level) and view past reports or check out recommended
+                        strategies for improving their sleep health. Admins get
+                        a bird's eye view of the entire company's sleep health
+                        and can add or delete users as employees come and go
+                        from the company.
                     </p>
                 </div>
                 <h5 className="text-3xl font-bold text-center">
