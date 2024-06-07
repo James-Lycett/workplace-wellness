63363265a78933c57485dee637b4abee162a147a
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-06-07 11:22:26 -0400

    Ajusted size of popover

--- frontend/src/LoginPage/LoginPage.jsx
+++ frontend/src/LoginPage/LoginPage.jsx
@@ -102,7 +102,7 @@ export default function LoginPage() {
                                         data-popover
                                         id="popover-default"
                                         role="tooltip"
-                                        className="absolute z-10 inline-block w-64 text-sm text-gray-500 bg-white border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800"
+                                        className="absolute z-10 inline-block w-2/3 md:w-1/4 text-sm text-gray-500 bg-white border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800"
                                     >
                                         <div class="flex justify-between px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
                                             <h3 class="font-semibold text-gray-900 dark:text-white">
