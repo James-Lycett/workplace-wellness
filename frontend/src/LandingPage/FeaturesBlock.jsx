c43cfc7bbda9a3837160aa22b133e46ce906cc0b
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-06-12 12:20:48 -0400

    Update responsiveness of Features Block
    made it so it's columns on mobile,
    Rows for tablet
    and columns for large screens again

--- frontend/src/LandingPage/FeaturesBlock.jsx
+++ frontend/src/LandingPage/FeaturesBlock.jsx
@@ -6,33 +6,33 @@ import stretchingGraphic from './stretchingGraphic.png'
 export default function FeaturesBlock() {
     return (
         <div className="flex lg:flex-row flex-col lg:gap-16 lg:w-4/5 lg:p-12 lg:text-xl font-regular">
-            <div className="flex lg:flex-col flex-row items-center flex-1 lg:m-0 m-4">
+            <div className="flex flex-col lg:flex-col sm:flex-row items-center flex-1 lg:m-0 m-4">
                 <img
                     src={sleepingGraphic}
-                    className="lg:w-full w-1/3 max-w-full"
+                    className="lg:w-full sm:w-1/3 w-full px-5 sm:p-0 max-w-full"
                 />
-                <p className="lg:mt-5">
+                <p className="lg:mt-5 pt-5">
                     We can help you sleep if you are interested in that.
                     Probably you are, cause you sleep a lot like a third of your
                     life or something.
                 </p>
             </div>
-            <div className="flex lg:flex-col flex-row-reverse items-center flex-1 lg:m-0 m-4">
+            <div className="flex flex-col lg:flex-col sm:flex-row-reverse items-center flex-1 lg:m-0 m-4">
                 <img
                     src={meditatingGraphic}
-                    className="lg:w-full w-1/3 max-w-full"
+                    className="lg:w-full sm:w-1/3 w-full px-5 sm:p-0 max-w-full"
                 />
-                <p className="lg:mt-5">
+                <p className="lg:mt-5 pt-5">
                     Also meditation is good for your brain, and your brain is
                     pretty important, and therefore handy to pay attention to.
                 </p>
             </div>
-            <div className="flex lg:flex-col flex-row items-center flex-1 lg:m-0 m-4">
+            <div className="flex flex-col lg:flex-col sm:flex-row items-center flex-1 lg:m-0 m-4">
                 <img
                     src={stretchingGraphic}
-                    className="lg:w-full w-1/3 max-w-full"
+                    className="lg:w-full sm:w-1/3 w-full px-5 sm:p-0 max-w-full"
                 />
-                <p className="lg:mt-5">
+                <p className="lg:mt-5 pt-5">
                     Exercise is good, but gyms aren't particularly inspiring so
                     we're showing you yoga. I dunno.
                 </p>
