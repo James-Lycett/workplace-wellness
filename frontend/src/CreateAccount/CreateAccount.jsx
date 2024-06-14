fc354724f810b342a61e382449da8aa8ab894fd8
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-06-14 11:12:14 -0400

    Updated CreateAccount to look like login page

--- frontend/src/CreateAccount/CreateAccount.jsx
+++ frontend/src/CreateAccount/CreateAccount.jsx
@@ -2,7 +2,6 @@ import React, { useCallback, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { createUser } from '../utils/api'
 
-
 export default function CreateAccount() {
     const [userType, setUserType] = useState('unselected')
     const navigate = useNavigate() // Initialize useHistory hook
@@ -36,22 +35,19 @@ export default function CreateAccount() {
         }
     }
 
-    const submitUser = useCallback(
-        async (user) => {
-            const abortController = new AbortController()
+    const submitUser = useCallback(async (user) => {
+        const abortController = new AbortController()
 
-            try {
-                const response = await createUser(user, abortController.signal)
-                return response
-            } catch (error) {
-                console.error(error)
-                throw error
-            } finally {
-                abortController.abort()
-            }
-        },
-        []
-    )
+        try {
+            const response = await createUser(user, abortController.signal)
+            return response
+        } catch (error) {
+            console.error(error)
+            throw error
+        } finally {
+            abortController.abort()
+        }
+    }, [])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -72,13 +68,13 @@ export default function CreateAccount() {
     }
 
     // Form field conditional renders
-    let eidField = ""
-    if (userType === "admin") {
+    let eidField = ''
+    if (userType === 'admin') {
         eidField = (
             <>
                 <label htmlFor="ein"></label>
                 <input
-                    className="relative my-2 py-2 px-2 w-full rounded border-2"
+                    className="relative border-0 bg-slate-100 my-4 py-3 px-2 w-full rounded max-w-2xl"
                     type="text"
                     id="ein"
                     name="ein"
@@ -88,85 +84,90 @@ export default function CreateAccount() {
                 />
             </>
         )
-      }
-
-
+    }
 
     return (
         <div className="flex flex-col items-center justify-center">
             <h1 className="mt-8 text-3xl font-bold">
-                You"re On the Journey to Better Mental Health
+                You're On the Journey to Better Mental Health
             </h1>
             <div
-                className="mt-5 mb-2 block w-1/2 rounded border-2 border-black px-6 pb-[6px] pt-2 text-s font-medium leading-normal text-primary"
+                className="mt-5 mb-2 block w-1/2 px-6 pb-[6px] pt-2 text-s font-medium leading-normal text-primary"
                 data-te-ripple-init
             >
                 <form onSubmit={handleSubmit}>
                     <div className="flex flex-col items-center justify-center">
                         {/* Select user type */}
-                            <label htmlFor="userType"></label>
-                            <select
-                            className=" mt-8 mb-2 py-2 w-full rounded border-2"
-                                name="dropdown"
-                                id="userType"
-                                onChange={handleChange}
-                            >
-                                <option value="unselected">
-                                    Select Type of Account
-                                </option>
-                                <option value="admin">Admin</option>
-                                <option value="user">User</option>
-                            </select>
+                        <label htmlFor="userType"></label>
+                        <select
+                            className="relative border-0 bg-slate-100 my-4 py-3 px-2 w-full rounded max-w-2xl"
+                            name="dropdown"
+                            id="userType"
+                            onChange={handleChange}
+                        >
+                            <option value="unselected">
+                                Select Type of Account
+                            </option>
+                            <option value="admin">Admin</option>
+                            <option value="user">User</option>
+                        </select>
                         {/* Username input */}
-                            <label htmlFor="username"></label>
-                            <input
-                                className="relative my-2 py-2 px-2 w-full rounded border-2"
-                                type="text"
-                                id="username"
-                                name="username"
-                                required={true}
-                                value={user.username}
-                                placeholder={userType === 'user' || userType === 'unselected' ? "Enter Username" : "Legal First and Last Name"}
-                                onChange={handleChange}
-                            />
-                            {eidField}
+                        <label htmlFor="username"></label>
+                        <input
+                            className="relative border-0 bg-slate-100 my-4 py-3 px-2 w-full rounded max-w-2xl"
+                            type="text"
+                            id="username"
+                            name="username"
+                            required={true}
+                            value={user.username}
+                            placeholder={
+                                userType === 'user' || userType === 'unselected'
+                                    ? 'Enter Username'
+                                    : 'Legal First and Last Name'
+                            }
+                            onChange={handleChange}
+                        />
+                        {eidField}
                         {/* Password input */}
-                            <label htmlFor="password"></label>
-                            <input
-                            className="relative my-2 py-2 px-2 w-full rounded border-2"
-                                type="password"
-                                id="password"
-                                name="password"
-                                value={password}
-                                placeholder={userType === 'admin' ? "Location Password" : "Password"}
-                                onChange={handleChange}
-                            />
-                        {
-                            (userType === 'user') &&
+                        <label htmlFor="password"></label>
+                        <input
+                            className="relative border-0 bg-slate-100 my-4 py-3 px-2 w-full rounded max-w-2xl"
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={password}
+                            placeholder={
+                                userType === 'admin'
+                                    ? 'Location Password'
+                                    : 'Password'
+                            }
+                            onChange={handleChange}
+                        />
+                        {userType === 'user' && (
                             <>
-                            <label htmlFor="confirm-password"></label>
-                            <input
-                                className="relative my-2 py-2 px-2 w-full rounded border-2"
-                                type="password"
-                                id="confirm-password"
-                                name="confirm-password"
-                                value={passwordConfirm}
-                                placeholder={"Confirm Password"}
-                                onChange={handleChange}
-                            />
+                                <label htmlFor="confirm-password"></label>
+                                <input
+                                    className="relative border-0 bg-slate-100 my-4 py-3 px-2 w-full rounded max-w-2xl"
+                                    type="password"
+                                    id="confirm-password"
+                                    name="confirm-password"
+                                    value={passwordConfirm}
+                                    placeholder={'Confirm Password'}
+                                    onChange={handleChange}
+                                />
                             </>
-                        }
+                        )}
                         {/* Register button */}
-                            <div className="flex flex-col items-center justify-center w-3/4">
-                                <button
-                                    type="submit"
-                                    className="button-dark-rounded mt-2 w-1/2"
-                                >
-                                    REGISTER
-                                </button>
-                            </div>
-                        <p>
-                            <a href="/login" className="underline">
+                        <div className="flex flex-col items-center justify-center max-w-52 mt-5">
+                            <button
+                                type="submit"
+                                className="w-full text-xl font-bold mx-20 button-light-blue"
+                            >
+                                REGISTER
+                            </button>
+                        </div>
+                        <p className="my-5">
+                            <a href="/login" className="underline ">
                                 Sign In
                             </a>
                         </p>
