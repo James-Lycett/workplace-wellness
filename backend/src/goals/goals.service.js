505a7fc08336fd399691673396787e1cc4856da8
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-06-20 16:56:29 -0400

    update function

--- backend/src/goals/goals.service.js
+++ backend/src/goals/goals.service.js
@@ -15,8 +15,15 @@ function read (personId) {
     return knex('goals').select('*').where({ person_id: personId }).first()
 }
 
+function update (updatedGoalsData) {
+    return knex('goals')
+        .where({ person_id: updatedGoalsData.person_id })
+        .update(updatedGoalsData, '*')
+}
+
 module.exports = {
     list,
     create,
     read,
+    update,
 }
