a2033272cff7232bc0d8f54c5a63bd628d8e3a06
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-06-20 16:51:45 -0400

    create function

--- backend/src/goals/goals.service.js
+++ backend/src/goals/goals.service.js
@@ -3,3 +3,15 @@ const knex = require('../db/connection')
 function list () {
     return knex('goals').select('*').orderBy('person_id')
 }
+
+function create (newGoalsData) {
+    return knex('goals')
+        .insert(newGoalsData)
+        .returning('*')
+        .then(createdRecords => createdRecords[0])
+}
+
+module.exports = {
+    list,
+    create,
+}
