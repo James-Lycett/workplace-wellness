30a08e13bc940692bd104eb58565f7478f701dab
James-Lycett <jrlycett@mail.com>
2024-06-19 20:19:36 -0700

    added NotFound page for 404 routes

--- frontend/src/Layout/routes.jsx
+++ frontend/src/Layout/routes.jsx
@@ -9,6 +9,7 @@ import Careers from '../Boilerplate/Careers'
 import Contact from '../Boilerplate/Contact'
 import Terms from '../Boilerplate/Terms'
 import PrivacyPolicy from '../Boilerplate/Privacy'
+import NotFound from '../NotFound/NotFound'
 import { TipsSleep, TipsMed, TipsFit } from '../Tips/TipsPages'
 
 export default function RoutesComponent() {
@@ -30,6 +31,7 @@ export default function RoutesComponent() {
                 <Route path="terms" element={<Terms />} />
                 <Route path="privacy" element={<PrivacyPolicy />} />
             </Route>
+            <Route path="*" element={<NotFound />}/>
         </Routes>
     )
 }
