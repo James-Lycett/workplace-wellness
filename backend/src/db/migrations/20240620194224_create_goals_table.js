25df808fdca70d30644ba81c9a7e4c2bc64601db
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-06-20 16:04:12 -0400

    updated the goals migration file

--- backend/src/db/migrations/20240620194224_create_goals_table.js
+++ backend/src/db/migrations/20240620194224_create_goals_table.js
@@ -2,14 +2,26 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+    return knex.schema.createTable('goals', function (table) {
+        table.integer('person_id').unsigned().primary()
+        table
+            .foreign('person_id')
+            .references('person_id')
+            .inTable('health_data')
+            .onDelete('CASCADE')
+        table.decimal('sleep_duration')
+        table.integer('quality_of_sleep')
+        table.integer('physical_activity_level')
+        table.integer('stress_level')
+        table.integer('daily_steps')
+    })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('goals')
+}
