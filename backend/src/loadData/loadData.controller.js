fc4ba9db402bd9dd107074352acde5820ca5ffcb
James-Lycett <jrlycett@mail.com>
2024-06-19 19:38:44 -0700

    backend error handling is nice and uniform now

--- backend/src/loadData/loadData.controller.js
+++ backend/src/loadData/loadData.controller.js
@@ -1,52 +1,61 @@
-const entriesController = require("../entries/entries.controller")
-const dataService = require("../data/data.service")
-const entriesService = require("../entries/entries.service")
-const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
-const authenticateToken = require("../authentication/authenticateToken")
-
+const entriesController = require('../entries/entries.controller')
+const dataService = require('../data/data.service')
+const entriesService = require('../entries/entries.service')
+const asyncErrorBoundary = require('../errors/asyncErrorBoundary')
+const authenticateToken = require('../authentication/authenticateToken')
 
 async function loadUserData(req, res, next) {
     const { userId } = req.params
 
     const { personIdFromToken } = req.user
-    if (personIdFromToken !== userId ) {
-        return next({ 
+    if (personIdFromToken !== userId) {
+        return next({
             status: 403,
-            message: "Forbidden: You do not have access to this user's data"
+            message: "Forbidden: You do not have access to this user's data",
         })
     }
 
-    const user = await dataService.read(userId)
-    const entries = await entriesService.readPerson(userId)
-    const averages = await entriesService.lastMonthAverages(userId)
+    try {
+        const user = await dataService.read(userId)
+        const entries = await entriesService.readPerson(userId)
+        const averages = await entriesService.lastMonthAverages(userId)
 
-    let data = {
-        user: user,
-        averages: averages,
-        entries: entries
-    }
-
-    if (user.admin) {
-        // lastMonthCompanyMetrics is only available to admins
-        if (!req.user.adminFromToken) {
-            return next({ 
-                status: 403,
-                message: "Forbidden: You do not have access to this user's data"
-            })
+        let data = {
+            user: user,
+            averages: averages,
+            entries: entries,
         }
-        const companyMetrics = await entriesController.lastMonthCompanyMetrics()
-        const employees = await dataService.list()
 
-        data = {
-            ...data,
-            companyMetrics: companyMetrics,
-            employees: employees
+        if (user.admin) {
+            // lastMonthCompanyMetrics is only available to admins
+            if (!req.user.adminFromToken) {
+                return next({
+                    status: 403,
+                    message:
+                        "Forbidden: You do not have access to this user's data",
+                })
+            }
+            const companyMetrics =
+                await entriesController.lastMonthCompanyMetrics()
+            const employees = await dataService.list()
+
+            data = {
+                ...data,
+                companyMetrics: companyMetrics,
+                employees: employees,
+            }
         }
-    }
 
-    res.status(200).json({ data })
+        res.status(200).json({ data })
+    } catch (error) {
+        console.error(error)
+        return next({
+            status: 500,
+            message: 'Error loading user data',
+        })
+    }
 }
 
 module.exports = {
-    loadUserData: [authenticateToken, asyncErrorBoundary(loadUserData)]
+    loadUserData: [authenticateToken, asyncErrorBoundary(loadUserData)],
 }
