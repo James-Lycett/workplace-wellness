920155d796fda6aa55f718fd91403704bb40c93d
James-Lycett <jrlycett@mail.com>
2024-06-19 19:43:00 -0700

    reformatted authenticateToken error handling too

--- backend/src/authentication/authenticateToken.js
+++ backend/src/authentication/authenticateToken.js
@@ -6,15 +6,19 @@ function authenticateToken(req, res, next) {
     const token = authHeader && authHeader.split(' ')[1]
 
     if (!token) {
-        return res.status(401).json({ message: "No auth token" })
+        return next({
+            status: 401,
+            message: "No auth token"
+        })
     }
 
     // Unencrypts the token from the frontend using the same secret key that it was encrypted with when originally sent to the frontend from login.controller
     jwt.verify(token, process.env.API_SECRET_KEY, (err, decodedToken) => {
         if (err) {
-            return res
-                .status(401)
-                .json({ message: "Unauthorized token" })
+            return next({
+                statu: 401,
+                message: "Unauthorized token"
+            })
         }
 
         // Stores the user's ID and username which the token has encoded so the ID and username can be used by downstream middleware
