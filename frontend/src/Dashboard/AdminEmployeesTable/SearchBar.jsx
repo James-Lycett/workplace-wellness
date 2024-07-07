1657fca08e9936e3e76234ac6c443a5e76e2bca6
James-Lycett <jrlycett@mail.com>
2024-07-07 13:17:46 -0700

    made searchBar non case-sensitive

--- frontend/src/Dashboard/AdminEmployeesTable/SearchBar.jsx
+++ frontend/src/Dashboard/AdminEmployeesTable/SearchBar.jsx
@@ -4,14 +4,14 @@ export default function SearchBar({ employees, setFilteredEmployees }) {
     const [searchString, setSearchString] = useState("")
 
     const filterEmployees = useCallback((value) => {
-        return employees.filter((employee) => employee.username.startsWith(value));
+        return employees.filter((employee) => employee.username.startsWith(value))
     }, [employees])
 
     // Run of the mill text field change handler with the addition of a simple search function executed on each letter typed
     const handleChange = (event) => {
         const { value } = event.target
         setSearchString(value)
-        setFilteredEmployees(filterEmployees(value))
+        setFilteredEmployees(filterEmployees(value.toLowerCase()))
     }
 
     return (
