8a7daf743afbfb4ad386ee5dab8e9d13c438ff21
James-Lycett <jrlycett@mail.com>
2024-08-22 18:15:21 -0700

    re-styled SummaryBar stat bubbles to match figma. Used placeholders in lieu of actual stats

--- frontend/src/Dashboard/SummaryBar/SummaryBar.jsx
+++ frontend/src/Dashboard/SummaryBar/SummaryBar.jsx
@@ -21,7 +21,7 @@ export default function SummaryBar({ user, averages }) {
 
     return (
         <>
-            <div className="flex flex-row md:flex-col bg-white rounded-lg shadow-md md:h-44 my-5 mx-3 md:mx-auto max-w-5xl">
+            <div className="flex flex-row md:flex-col bg-white rounded-lg shadow-md md:h-60 my-5 mx-3 md:mx-auto md:mb-10 max-w-5xl">
                 <div className="flex flex-col md:flex-row w-1/2 md:w-full md:self-center p-2.5">
                     <img
                         src={profilePic}
@@ -32,28 +32,28 @@ export default function SummaryBar({ user, averages }) {
                         <h3 className="text-v2-drkblue">{user.occupation}</h3>
                     </div>
                 </div>
-                <div className="flex flex-col py-5 px-3 sm:px-8 md:p-0 md:flex-row items-center justify-between gap-5 md:gap-16 w-full md:h-24">
-                    <div className="flex items-center justify-center bg-white w-full rounded-full shadow-md h-9">
+                <div className="flex flex-col py-5 px-3 sm:px-8 md:p-0 md:mx-2 md:flex-row items-center justify-between gap-5 md:gap-10 w-100% md:h-24">
+                    <div className="flex items-center justify-center bg-white w-full rounded-3xl shadow-lg h-20">
                         <h3 className="text-sm md:text-lg">
-                            Average Sleep Hours:{' '}
-                            <span className="font-semibold ps-2 ">
-                                {Math.round(averages.sleep_duration_average)}
+                            Monthly Goals Met:{' '}
+                            <span className="font-semibold ps-2 md:text-2xl">
+                                3/6
                             </span>
                         </h3>
                     </div>
-                    <div className="flex items-center justify-center bg-white w-full rounded-full shadow-md h-9">
+                    <div className="flex items-center justify-center bg-white w-full rounded-3xl shadow-lg h-20">
                         <h3 className="text-sm md:text-lg">
-                            Average Stress Level:{' '}
-                            <span className="font-semibold ps-2">
-                                {Math.round(averages.stress_level_average)}
+                            % of Dept Goals:{' '}
+                            <span className="font-semibold ps-2 md:text-2xl">
+                                81%
                             </span>
                         </h3>
                     </div>
-                    <div className="flex items-center justify-center bg-white w-full rounded-full shadow-md h-9">
+                    <div className="flex items-center justify-center bg-white w-full rounded-3xl shadow-lg h-20">
                         <h3 className="text-sm md:text-lg">
-                            Average Daily Steps:{' '}
-                            <span className="font-semibold ps-2">
-                                {Math.round(averages.daily_steps_average)}
+                            Longest Goal Streak:{' '}
+                            <span className="font-semibold ps-2 md:text-2xl">
+                                
                             </span>
                         </h3>
                     </div>
